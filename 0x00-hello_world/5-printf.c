e2fbfcf6fcc599be2c6bdc1f656427167d9f0000
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-10 17:01:19 +0100

    Using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<conio.h>
+/**
+ * Program entry
+ *
+ * Description: 'This program prints few phrase on the screen using printf function'
+ *
+ */
+int main(void)
+{
+
+	Printf(" with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
