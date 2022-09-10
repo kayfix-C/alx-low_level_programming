a5e599deda5dd08253067547f5b8ec38a954db71
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-10 16:01:47 +0100

    Using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main()
+int main(void) /*This is where program excution starts*/
 {
 	/*This program prints some facts about programming on the stout*/
 	char kayFix[] = "\"Programming is like building a multilingual puzzle";
-	puts(kayFix) ;
+	puts(kayFix); 
 	return (0);
 }
