1319eaeecd870ef60a65af6fae6ca4f77894964d
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-10 04:39:50 -0700

    Printing to stout using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	/*This program prints some facts about programming on the stout*/
-	char kayFix[] = "Programming is like building a multilingual puzzle";
+	char kayFix[] = "\"Programming is like building a multilingual puzzle";
 	puts(kayFix);
 	return 0;
 }
