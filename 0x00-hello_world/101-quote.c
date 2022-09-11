375389f39da950a0f1c9cb0d50d4aa5f2273c060
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-11 16:29:27 +0100

    Tired

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  *
  * Description: 'Printing to the stderr without common functions'
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
 
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n",2,err,59);
+	write(2, err, 59);
+
 	return (1);
 }
