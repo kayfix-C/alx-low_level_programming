c21c95607648482c26050678acfb60961250788e
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-11 08:51:21 +0100

    intel code

---
+++ 0x00-hello_world/intel.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * global variable declaration
+ *
+ * main - Entry point
+ *
+ * Description: 'Intel Inline Assemble Code'
+ *
+ * Return: Always 0 (Success)
+ * 
+ */
+unsigned int i = 0;
+
+int main()
+{
+    printf("%d\n", i);
+    asm(".intel_syntax noprefix\n");
+    asm("mov dword ptr [i], 1\n");
+    printf("%d\n", i);
+
+    return 0;
+}
