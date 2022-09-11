d0adaa05dd9684ed4d38abc155acc64aba178a34
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-12 00:34:18 +0100

    double wahala for deadi bodi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,10 +8,8 @@
  */
 int main(void)
 {
-	/*This program prints some facts about programming on the stout*/
 	char kayFix[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(kayFix);
-
 	return (0);
 }
