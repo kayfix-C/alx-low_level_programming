8c0d9262e76cfb8d4c0a869923c98b1e22f62e6b
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-12 16:58:28 +0100

    Corrections of lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,18 +3,17 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	char low;
 
 	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
+	putchar(low);
+	putchar('\n');
 	return (0);
 }
