3e12e87f09e6fb749b9a3f0ce3f5019ce9ab56b4
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-12 17:28:19 +0100

    101-quote corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry Point
  *
  * Description: prints a string to the standard error
  *
- * Return: Always 1(Error)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
-	write(2,  err, 59);
 	return (1);
 
 }
