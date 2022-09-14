69d05f273e030472a02f9406a38231849bd2666e
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-14 03:17:02 +0100

    Prints a-z x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Uses _putchar function to print
+ * small alphabets ten times
+ *
+ * Return: Always 0 (Success).
+ */
+
+void print_alphabet_x10(void)
+{
+	char k;
+	int a;
+
+	for (a = 0; a < 10; j += 1)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(k);
+	}
+
+		_putchar('\n');
+	}
+}
