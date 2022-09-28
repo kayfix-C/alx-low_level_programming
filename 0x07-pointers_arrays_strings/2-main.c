8618319c52a1f429597ffd8be436d7e3e429a14d
Waliyullahi Zakariyah <holuwakayor@gmail.com>
2022-09-28 04:21:16 +0100

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
+}
